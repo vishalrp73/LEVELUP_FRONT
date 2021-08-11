29874ad021adbff5227a9e4c8219de481ec08abd
Vishal Prakash <v.r.prakash73@gmail.com>
2021-08-12 10:33:39 +1200

    added instructional comments on app.js for next steps

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import CorePanel from './components/core/corePanel/corePanel';
 import InfoSelect from './components/core/infoSelect/infoSelect';
 import Entry from './components/core/entry/entry';
 
+/* THIS APPLICATION IS TEMPORARY UNTIL MERGING INTO MAIN BRANCH*/
+/* NEED TO ORDER FILES INTO DISTINCT FOLDERS AND RE-IMPORT FILE PATHS
+    FOR INDIVIDUAL PAGES */
+
 
 function App() {
   return (
