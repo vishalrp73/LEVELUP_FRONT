53b1975986d8b56d32d84d1de5631aae1ee57384
Vishal Prakash <v.r.prakash73@gmail.com>
2021-08-20 18:30:12 +1200

    restructuring some files, adding pages folder and re-organising paths

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import './App.css';
-import Header from './components/head-foot/header';
-import Footer from './components/head-foot/footer';
-import CoreBanner from './components/core/coreBanner/coreBanner';
-import CoreSlides from './components/core/coreSlides/coreSlides';
-import CorePanel from './components/core/corePanel/corePanel';
-import InfoSelect from './components/core/infoSelect/infoSelect';
-import Entry from './components/core/entry/entry';
-
-/* THIS APPLICATION IS TEMPORARY UNTIL MERGING INTO MAIN BRANCH*/
-/* NEED TO ORDER FILES INTO DISTINCT FOLDERS AND RE-IMPORT FILE PATHS
-    FOR INDIVIDUAL PAGES */
-
+import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <CoreBanner />
-      <CoreSlides />
-      <CorePanel />
-      <InfoSelect />
-      <Entry />
-      <Footer />
+      <Home />
     </div>
   );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,23 @@
+import Header from '../components/head-foot/header';
+import Footer from '../components/head-foot/footer';
+import CoreBanner from '../components/core/coreBanner/coreBanner';
+import CoreSlides from '../components/core/coreSlides/coreSlides';
+import CorePanel from '../components/core/corePanel/corePanel';
+import InfoSelect from '../components/core/infoSelect/infoSelect';
+import Entry from '../components/core/entry/entry';
+
+const Home = () => {
+    return(
+        <>
+            <Header />
+            <CoreBanner />
+            <CoreSlides />
+            <CorePanel />
+            <InfoSelect />
+            <Entry />
+            <Footer />
+        </>
+    )
+}
+
+export default Home;
