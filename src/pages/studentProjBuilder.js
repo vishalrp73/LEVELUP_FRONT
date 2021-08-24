0b8997fb098d9dfe3ae1380a8b4ce7031d9d0f56
Vishal Prakash <v.r.prakash73@gmail.com>
2021-08-24 15:20:02 +1200

    adding basic student side project builder page

---
+++ src/pages/studentProjBuilder.js
@@ -0,0 +1,11 @@
+import './styles/StudentProjectBuilder.css';
+
+const StudentProjectBuilder = () => {
+    return (
+        <>
+            <p>Student Project Builder</p>
+        </>
+    )
+}
+
+export default StudentProjectBuilder;
